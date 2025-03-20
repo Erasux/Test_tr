5324048af2f8c6b645b3149dad762c99272d2365
Erasux <samirgg2000@gmail.com>
2025-03-20 18:32:34 -0500

    Updated App vue

--- Frontend/stock-tracker/src/App.vue
+++ Frontend/stock-tracker/src/App.vue
@@ -1,17 +1,10 @@
-<script setup>
-import StockList from "./components/StockList.vue";
-import StockRecommendations from "./components/StockRecommendations.vue";
-</script>
-
 <template>
-  <div class="container mt-4">
-    <!-- Tabla de Todas las Acciones -->
-    <StockList />
-
-    <!-- Espaciado entre tablas -->
-    <div class="my-5"></div>
-
-    <!-- Tabla de Recomendaciones -->
-    <StockRecommendations />
+  <div>
+    <Navbar />
+    <router-view></router-view>
   </div>
 </template>
+
+<script setup lang="ts">
+import Navbar from "@/components/Navbar.vue";
+</script>
