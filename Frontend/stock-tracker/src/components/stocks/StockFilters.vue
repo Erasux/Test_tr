3c2e618acd4eb84cc2ebfa1f6250c62375aa76fb
Erasux <samirgg2000@gmail.com>
2025-03-20 18:48:59 -0500

    Changed Styles for filters

--- Frontend/stock-tracker/src/components/stocks/StockFilters.vue
+++ Frontend/stock-tracker/src/components/stocks/StockFilters.vue
@@ -6,7 +6,7 @@
             class="p-2 border border-gray-300 rounded shadow-sm focus:ring focus:ring-blue-300" />
         <input v-model="filters.rating" placeholder="Search by Rating"
             class="p-2 border border-gray-300 rounded shadow-sm focus:ring focus:ring-blue-300" />
-        <button @click="applyFilters" class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition">
+        <button @click="applyFilters" class="px-4 py-2 bg-gray-500 text-white rounded hover:bg-gray-600 transition">
             Apply Filters
         </button>
     </div>
