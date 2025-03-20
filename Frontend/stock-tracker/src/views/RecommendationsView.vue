d94b1326d2a4bdf91dd8341f353164cf9bb23ee8
Erasux <samirgg2000@gmail.com>
2025-03-20 18:38:03 -0500

    Created view for StockRecommendation

---
+++ Frontend/stock-tracker/src/views/RecommendationsView.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="container mx-auto p-6 bg-gray-50 min-h-screen">
+        <h1 class="text-3xl font-bold mb-6 text-center text-gray-800">📈 Stock Recommendations</h1>
+        <StockRecommendations />
+    </div>
+</template>
+
+<script setup lang="ts">
+import StockRecommendations from "../components/stocks/StockRecommendations.vue";
+</script>
